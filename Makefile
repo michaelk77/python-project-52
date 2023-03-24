f8a44a2db5b90c600013d26e37e509e39fd5935c
michael <catcat2005@inbox.ru>
2023-03-24 17:18:21 +0100

    add linter and fixes

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@ dev:
 migrate:
 	poetry run python manage.py makemigrations
 	poetry run python manage.py migrate
+
+lint:
+	poetry run flake8 task_manager/
--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,6 +4,6 @@ from .views import Users, UserCrate, UserUpdate, UserDelete
 urlpatterns = [
     path('', Users.as_view(), name='users'),
     path('create/', UserCrate.as_view(), name='sign_up'),
-    path('update/<int:pk>/', UserUpdate.as_view(), name='user_update'),
-    path('delete/<int:pk>/', UserDelete.as_view(), name='user_delete'),
+    path('<int:pk>/update/', UserUpdate.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDelete.as_view(), name='user_delete'),
 ]
